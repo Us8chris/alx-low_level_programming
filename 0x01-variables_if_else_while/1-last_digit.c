9890f02f509467370a65b3e96220367f34e862cb
us8chris <usorochris29@gmail.com>
2023-07-17 18:43:17 -0700

    - [Got]

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Print if number is positive, negative or zero
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 
 int main(void)
